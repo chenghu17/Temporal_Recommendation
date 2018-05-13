1712896e68adeddc315ffd08c3e287c0bfbdaafc
CallmeMrhu <mrhu666@163.com>
2018-05-13 19:33:59 +0800

    commit

--- evolution.py
+++ evolution.py
@@ -2,8 +2,8 @@ import metric
 import os
 
 if __name__ == '__main__':
-    # rootPath = 'data_LastFM/'
-    rootPath = 'data_Epinions/'
+    rootPath = 'data_LastFM/'
+    # rootPath = 'data_Epinions/'
     # rootPath = 'data_FineFoods/'
     # rootPath = 'data_Netflix/'
     # rootPath = 'data_MovieLen/'
@@ -11,20 +11,23 @@ if __name__ == '__main__':
     validationPath = rootPath + 'validation.tsv'
     testPath = rootPath + 'test.tsv'
     alpha = 0.02
-    alpha_Reg = 0.02
-    gama = 0.02
-    # resultPath = 'alpha_' + str(alpha) + '_alphaReg_' + str(alpha_Reg) + '_gama_' + str(gama)+'/'
-    resultPath = 'dpf/'
-    rootPath = rootPath + resultPath
+    alpha_Reg = 0.1
+    # gama = 0.02
+    gama = 0.0002
+    resultPath = 'alpha_' + str(alpha) + '_alphaReg_' + str(alpha_Reg) + '_gama_' + str(gama)+'/'
+    # resultPath = 'dpf/'
+    # state = 'dynamic/'
+    state = 'static/'
+    rootPath = rootPath + state + resultPath
     Max = 100  # 给每个user预测Max个item，用于计算后面的评价标准
-    K = 100  # precision@K，K可以取10，50，100，不要大于Max
+    K = 10  # precision@K，K可以取10，50，100，不要大于Max
     timestep = 9  # 每个时间间隔跨度，即main_running.py中的interval，取值为3，6，9
     # for lastfm
-    # n = 1000
-    # m = 1000
+    n = 1000
+    m = 1000
     # for epinions
-    n = 22579
-    m = 740029
+    # n = 22579
+    # m = 740029
     # for finefoods
     # n = 1892
     # m = 19489
@@ -35,8 +38,8 @@ if __name__ == '__main__':
     # n = 10702
     # m = 26231
     # 训练出来的user和item特征向量，内乘即为user对item的偏好，对结果进行排序，得到ranking.tsv,再计算以下评价标准
-    itemMat = 'itemMat4'
-    userMat = 'userMat4'
+    itemMat = 'itemMat12'
+    userMat = 'userMat12'
     # 判断之前是否已经生成ranking(Max).tsv
     exists = os.path.exists(rootPath + 'evolution' + str(timestep) + '/ranking' + str(Max) + '.tsv')
     if not exists:
